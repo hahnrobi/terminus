710389467a15bd798747603548bde77f5ac423b5
Hahn Robi <robitwo.r2@gmail.com>
2022-04-27 17:22:14 +0200

    Removed terminal theme from store.

--- store/index.js
+++ store/index.js
@@ -12,18 +12,11 @@ export default () => new Vuex.Store({
             right: true,
             rightDrawer: false,
         },
-        terminalTheme: ""
     },
     getters : {
         ownedShells: (state) => state.ownedShells,
         getOwnedShell: (state, shellId) => state.ownedShells.filter(_id === shellId),
         getLayoutParams: (state) => state.layoutParams,
-        getTerminalTheme: (state) => {
-            if (process.browser && !state.terminalTheme){
-                state.terminalTheme = localStorage.getItem("terminal-theme");
-            }
-            return state.terminalTheme;
-        }
     },
     mutations: {
         SET_OWNED_SHELLS(state, shells) {
@@ -32,10 +25,6 @@ export default () => new Vuex.Store({
         UPDATE_LAYOUT_PARAMS(state, {param, value}) {
             state.layoutParams[param] = value;
             console.log(state.layoutParams);
-        },
-        SET_TERMINAL_THEME(state, theme) {
-            state.terminalTheme = theme;
-            localStorage.setItem("terminal-theme", theme);
         }
     },
     actions: {
